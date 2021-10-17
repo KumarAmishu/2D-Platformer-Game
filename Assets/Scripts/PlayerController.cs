dbd40e84e698b4c117b7a0a4254ba6da91bc9bb6
KumarAmishu <kumar.amishu72@gmail.com>
2021-10-17 20:14:57 +0530

    Make crouch and jump animation
    on pressing Cntrl key on keyboard player crouches and we accepted the horizontal input when vertical > 0 then play jump animation.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,17 +14,69 @@ public class PlayerController : MonoBehaviour
   private void Update()
   
   {
-   
+
    float speed = Input.GetAxisRaw("Horizontal");
-   animator.SetFloat("Speed", Mathf.Abs(speed));
-   Vector3 scale = transform.localScale;
-   if(speed<0){
-       scale.x = -1f * Mathf.Abs(scale.x);
-   }
-   else if(speed>0){
-       scale.x = Mathf.Abs(scale.x);
-   } 
-   transform.localScale = scale;
-  }
    
-}
+   animator.SetFloat("Speed", Mathf.Abs(speed));
+   
+   Vector3 scale = transform.localScale;
+   
+   if(speed<0){
+    
+    scale.x = -1f * Mathf.Abs(scale.x);
+   
+   }
+
+   else if(speed>0){
+    
+    scale.x = Mathf.Abs(scale.x);
+   
+   } 
+   
+  float jump = Input.GetAxisRaw("Vertical");
+
+
+   if(jump > 0){
+
+    
+  animator.SetBool("jump", true) ;
+   
+   }
+   
+   else {
+    
+      animator.SetBool("jump", false) ;
+
+   
+    } 
+   
+
+   bool crouch = Input.GetKeyDown(KeyCode.LeftControl);
+   
+   bool c = Input.GetKeyUp(KeyCode.LeftControl);
+
+   if(crouch == true){
+
+    animator.SetBool("Crouch", true);
+   
+   }
+   
+   else if(c){
+    
+    animator.SetBool("Crouch", false);
+     
+   }
+
+   transform.localScale = scale;
+
+   }
+    
+  
+  }
+
+
+  
+
+ 
+   
+
